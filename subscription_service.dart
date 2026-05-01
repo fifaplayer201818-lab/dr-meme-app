9631da747908512f8c85e8486de2e61262454f63
fifaplayer201818-lab <fifaplayer201818@gmail.com>
2026-05-02 03:12:01 +0400

    Delete subscription_service.dart

--- subscription_service.dart
+++
@@ -1,9 +0,0 @@
-class SubscriptionService {
-  // Demo mode: change this to false to test locked subscription screen.
-  bool active = true;
-  DateTime subscriptionEndDate = DateTime.now().add(const Duration(days: 30));
-
-  bool get isActive => active && subscriptionEndDate.isAfter(DateTime.now());
-}
-
-final subscriptionService = SubscriptionService();
